4adfd442a15c8cd4d8bf698e1285d0d2992f578f
Shao Zhi <soongshaozhi@gmail.com>
2025-08-20 01:52:33 +0800

    Implemented echo chatbot

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Jimmy {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Jimmy");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("exit")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                running = false;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+            }
+            }
+        }
     }
-}
