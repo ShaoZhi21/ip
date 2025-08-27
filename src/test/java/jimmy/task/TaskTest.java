99cb5ff0906ec1e2bdd80bfadb27cbf1bcce7935
Shao Zhi <soongshaozhi@gmail.com>
2025-08-27 13:01:37 +0800

    Add task test

--- src/test/java/jimmy/task/TaskTest.java
+++ src/test/java/jimmy/task/TaskTest.java
@@ -1,4 +1,22 @@
 package jimmy.task;
 
-// Test file for Task class
-// Add your test methods here
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void testTask() {
+        Task task = new Task("Test Task");
+        assertEquals("Test Task", task.getDescription());
+    }
+    
+    @Test
+    public void testTaskStatusChanges() {
+        Task task = new Task("Another Task");
+        assertEquals(" ", task.getStatusIcon()); 
+        task.markAsDone();
+        assertEquals("X", task.getStatusIcon());    
+        task.markAsNotDone();
+        assertEquals(" ", task.getStatusIcon());
+    }
+}
