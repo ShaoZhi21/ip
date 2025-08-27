50b6033c3a211fc5ca4d99be45671161cc39e099
Shao Zhi <soongshaozhi@gmail.com>
2025-08-27 13:01:41 +0800

    Add todo test

--- src/test/java/jimmy/task/TodoTest.java
+++ src/test/java/jimmy/task/TodoTest.java
@@ -1,4 +1,20 @@
 package jimmy.task;
 
-// Test file for Todo class
-// Add your test methods here
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void testTodoCreation() {
+        Todo todo = new Todo("Buy groceries");
+        assertEquals("Buy groceries", todo.getDescription());
+        assertEquals(" ", todo.getStatusIcon()); 
+    }
+    
+    @Test
+    public void testTodoStatusChange() {
+        Todo todo = new Todo("Read book");
+        todo.markAsDone();
+        assertEquals("X", todo.getStatusIcon()); 
+    }
+}
