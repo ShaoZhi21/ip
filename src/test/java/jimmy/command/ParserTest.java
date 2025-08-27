ee92ccdcaa6b58a05b2384903ac963a1a0e384bb
Shao Zhi <soongshaozhi@gmail.com>
2025-08-27 13:01:07 +0800

    Add parser test

--- src/test/java/jimmy/command/ParserTest.java
+++ src/test/java/jimmy/command/ParserTest.java
@@ -1,4 +1,22 @@
 package jimmy.command;
 
-// Test file for Parser class
-// Add your test methods here
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import jimmy.exception.JimmyException;
+
+public class ParserTest {
+    @Test
+    public void testParseCommand() {
+        Parser.ParsedCommand parsed = Parser.parseCommand("todo buy milk");
+        assertEquals("todo", parsed.command);
+        assertEquals("buy milk", parsed.fullInput);
+    }
+    
+    @Test
+    public void testParseCommandWithNoArguments() {
+        Parser.ParsedCommand parsed = Parser.parseCommand("list");
+        assertEquals("list", parsed.command);
+        assertEquals("", parsed.fullInput);
+    }
+}
