df78c8da695e11e00d3d3d926f0917d21b99705b
Shao Zhi <soongshaozhi@gmail.com>
2025-08-27 13:01:29 +0800

    Add tasklist test

--- src/test/java/jimmy/task/TaskListTest.java
+++ src/test/java/jimmy/task/TaskListTest.java
@@ -1,4 +1,25 @@
 package jimmy.task;
 
-// Test file for TaskList class
-// Add your test methods here
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskListTest {
+    @Test
+    public void testTaskListCreation() {
+        List<Task> tasks = new ArrayList<>();
+        TaskList taskList = new TaskList(tasks);
+        assertEquals(0, taskList.getSize());
+    }
+    
+    @Test
+    public void testAddTask() {
+        TaskList taskList = new TaskList(new ArrayList<>());
+        Task task = new Task("Test task");
+        taskList.addTask(task);
+        assertEquals(1, taskList.getSize());
+        assertEquals(task, taskList.getTask(0));
+    }
+}
